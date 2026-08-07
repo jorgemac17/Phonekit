5109c33e0d17c341d46593997ab6073b256d2943
jorgemac17 <jorgemac17@example.com>
2026-08-07 12:40:38 -0300

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,1 @@
+document.addEventListener('submit',e=>{if(e.target.matches('[data-login]')){e.preventDefault();document.querySelector('[data-login-status]').textContent='Inicio de sesión simulado correctamente.'}});
